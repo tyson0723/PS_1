dafe2120616b24beab1befe588f54880085c5163
Tyson0723 <t.nagasaki.sys22@morijyobi.ac.jp>
2023-12-04 15:17:43 +0900

    hello,hey

--- scripts/my_scene.js
+++ scripts/my_scene.js
@@ -5,15 +5,15 @@ class MyScene extends Phaser.Scene {
     constructor() {
         super({ key: 'MyScene', active: true });
     }
-
-
     // シーンの事前読み込み処理
     preload() {
          // 画像の読み込み(使用する時の名前, パス)
         this.load.image('sky', 'assets/background.png');
         this.load.image('taro', 'assets/taro.png');
         this.load.image('jiro', 'assets/jiro.png');
+        this.load.image('hanako', 'assets/hanako.png');
     }
+
     // シーン初期化処理
     create() {
          // 単体画像をシーンに追加(X座標,Y座標,画像名)
@@ -24,6 +24,7 @@ class MyScene extends Phaser.Scene {
         this.text = this.add.text(10, 10, 'Scene 1').setFontSize(32).setColor('#ff0');
         this.text2 = this.add.text(600, 400, 'MyWorld').setFontSize(20).setColor('#ff0');
 
+
         this.helloText = this.add.text(100, 50, '');
         this.heyText = this.add.text(100, 50, '');
 
@@ -31,6 +32,7 @@ class MyScene extends Phaser.Scene {
         this.keys.keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
         this.keys.keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
         this.keys.keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+        this.keys.keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
         // this.taro.angle = 0;
         // this.player_direction = 1;
     }
@@ -59,14 +61,16 @@ class MyScene extends Phaser.Scene {
             this.taro.x += 50;
             this.jiro.x -= 50;
         }
-
         if(this.keys.keyA.isDown){
-            this.helloText.setText('Hello!').setFontSize(30);
+            this.helloText.setText('Hello!');
         }else if(this.keys.keyS.isDown){
-            this.heyText.setText('Hey!').setFontSize(30);
+            this.heyText.setText('Hey!');
         }else if(this.keys.keyD.isDown){
             this.helloText.setText('');
             this.heyText.setText('');
+        }else if(this.keys.keyW.isDown){
+            let randx = Phaser.Math.Between(100, 400);
+            this.hanako = this.add.image(randx, 100, 'hanako');
         }
     }
 
